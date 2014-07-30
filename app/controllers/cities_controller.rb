a5b4ccf70c06526d8e0587c73525fdd8cbc78211
Taha Jalil <taha.jalil@gmail.com>
2014-07-30 09:54:50 -0400

    created cities controller

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,36 @@
+class CitiesController < ApplicationController
+
+  before_filter :find_city, only: [:show]
+
+  def index
+    @cities = City.all
+  end
+
+  def new
+    @city = City.new
+  end
+
+  def create
+    @city = City.new(city_params)
+
+    if @city.save
+      redirect_to city_path(@city), notice: "#{@city.name} successfully created!"
+    else
+      render :new, alert: "There was an issue creating the city. Please try again."
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:name, :address, :background_img, :email, :phone, :facebook, :twitter, :eventbrite_page, :user_id)
+  end
+
+  def find_city
+    @city = City.find(params[:id])
+  end
+
+end
