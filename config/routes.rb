3b87c447e849dd62a4e5f9f9f12c016ab86f2b6d
Taha Jalil <taha.jalil@gmail.com>
2014-08-02 19:28:55 -0400

    set cities#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'cities#index', as: 'index'
+  root :to => 'cities#index'
 
   resources :cities, only: [:index, :show]
 
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
       get 'dekh_magar_pyar_say'
     end
   end
+
+  # post 'logout' => 'admin_sessions#destroy', :as => :logout
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
