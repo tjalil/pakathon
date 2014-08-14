1a11404f46f49ae6e681876b79b8e4eefc86d9bf
Taha Jalil <taha.jalil@gmail.com>
2014-08-14 19:38:20 -0400

    added uploader to appropriate columns in city and user models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,5 @@
 class City < ActiveRecord::Base
-
   has_many :users
   belongs_to :admin
-  
+  mount_uploader :background_img, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-
   belongs_to :city
-  
+  mount_uploader :avatar, ImageUploader
 end
