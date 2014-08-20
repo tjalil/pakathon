a72ae49cf0abb226c7c874a4502f4b17ce253cb9
Taha Jalil <taha.jalil@gmail.com>
2014-08-20 08:29:09 -0400

    added methods in City model to be used in the views

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,19 @@
 class City < ActiveRecord::Base
   has_many :users
   belongs_to :admin
+  has_many :contacts
   mount_uploader :background_img, ImageUploader
+
+  def judges
+    self.users.where(type_of_user: "Judge")
+  end
+
+  def team_members
+    self.users.where(type_of_user: "Team Member")
+  end
+
+  def mentors
+    self.users.where(type_of_user: "Mentor")
+  end
+
 end
