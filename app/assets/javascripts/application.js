9e8a14383f8dd7ba7f116d1c98874d84dc2ecebc
Taha Jalil <taha.jalil@gmail.com>
2014-09-01 05:20:34 -0400

    added layout.js as primary js file and required it in the manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require layout
 
 $(document).foundation();
---
+++ app/assets/javascripts/layout.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $('.video-action').mouseenter(function(){
+    $(this).hide();
+    $('.video-action-on-hover').show();
+  });
+
+  $('.video-action-on-hover').mouseleave(function(){
+    $(this).hide();
+    $('.video-action').show();
+  });
+
+  $('.video-action-on-hover').click(function(){
+    $('.hero-text').hide();
+    $('.video-action-on-click').fadeIn();
+  });
+
+});
