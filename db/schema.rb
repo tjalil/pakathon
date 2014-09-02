126f26ec418a84d4581b538afcbd7b78d0ac7881
Taha Jalil <taha.jalil@gmail.com>
2014-09-01 23:25:46 -0400

    added slug to cities to replace city id with city name in url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825040339) do
+ActiveRecord::Schema.define(version: 20140902031919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,10 @@ ActiveRecord::Schema.define(version: 20140825040339) do
     t.string   "province_name"
     t.string   "postal_code"
     t.text     "team_description"
+    t.string   "slug"
   end
+
+  add_index "cities", ["slug"], name: "index_cities_on_slug", using: :btree
 
   create_table "contacts", force: true do |t|
     t.string   "name"
