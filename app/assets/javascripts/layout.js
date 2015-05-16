d2bb84cae8f8e63f32976d1d11ae9afbb4452f14
Taha Jalil <taha.jalil@gmail.com>
2015-05-16 03:55:10 -0400

    completed header transition into solid color on scroll

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+  // header-transition
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 30) {
+      if (!$("header.global").hasClass('headerTransition')) {
+        $("header.global").addClass("headerTransition");
+      }
+    } else {
+      if ($("header.global").hasClass('headerTransition')) {
+        $("header.global").removeClass("headerTransition");
+      }
+    }
+  });
+
   $('.video-action').mouseenter(function(){
     $(this).hide();
     $('.video-action-on-hover').show();
