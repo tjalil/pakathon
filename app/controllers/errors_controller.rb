06d5053954484f76467709f5c9d299c3907c3e27
Taha Jalil <taha.jalil@gmail.com>
2015-06-10 23:06:52 -0400

    removed custom error page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,10 @@
 class ErrorsController < ApplicationController
-  def file_not_found
-    respond_to do |format|
-      format.html { render template: 'errors/file_not_found', layout: 'layouts/application', status: 404 }
-      format.all  { render nothing: true, status: 404 }
-    end
-  end
+  # def file_not_found
+  #   respond_to do |format|
+  #     format.html { render template: 'errors/file_not_found', layout: 'layouts/application', status: 404 }
+  #     format.all  { render nothing: true, status: 404 }
+  #   end
+  # end
 
   # def internal_server_error
   #   render :status => 500
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root :to => 'cities#index'
 
-  match '/404', to: 'errors#file_not_found', via: :all
+  # match '/404', to: 'errors#file_not_found', via: :all
   # match '/500', to: 'errors#internal_server_error', via: :all
 
   resources :cities, only: [:index]
