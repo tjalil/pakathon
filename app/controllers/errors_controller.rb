e04b3e091865e7b6fd5cb94126643ac1ae36c732
Taha Jalil <taha.jalil@gmail.com>
2015-06-10 23:59:18 -0400

    handling different formats in the 404 action in errors_controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,7 @@
 class ErrorsController < ApplicationController
   def file_not_found
     respond_to do |format|
-      format.html { render template: 'errors/file_not_found', layout: 'layouts/application', status: 404 }
+      format.any(:htm, :html, :xls, :xlsx) { render template: 'errors/file_not_found', layout: 'layouts/application', status: 404, formats: [:html] }
       format.all  { render nothing: true, status: 404 }
     end
   end
