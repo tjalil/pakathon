a5a8ec80d79b240d765434a1f5675e28019622a6
Taha Jalil <taha.jalil@gmail.com>
2015-08-31 20:29:35 -0400

    hackathon.rb: added image uploaders to images belonging to each day

--- app/models/hackathon.rb
+++ app/models/hackathon.rb
@@ -1,3 +1,7 @@
 class Hackathon < ActiveRecord::Base
   belongs_to :city
+
+  mount_uploader :day_1_image, ImageUploader
+  mount_uploader :day_2_image, ImageUploader
+  mount_uploader :day_3_image, ImageUploader
 end
