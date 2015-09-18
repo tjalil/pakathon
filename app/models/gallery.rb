a2bc7e5e90efd682076665adc0bf0e641e0f793b
Taha Jalil <taha.jalil@gmail.com>
2015-09-18 19:33:03 -0400

    views#galleries: added new and edit form for gallery images

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,8 @@
 class Gallery < ActiveRecord::Base
   belongs_to :city
+
+  mount_uploader :image_1, ImageUploader
+  mount_uploader :image_2, ImageUploader
+  mount_uploader :image_3, ImageUploader
+  mount_uploader :image_4, ImageUploader
 end
